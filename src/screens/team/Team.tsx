396a3c9b17264f5bb93876ddf22a8feba89f316d
CHAIBOU <sarahbchaibou@gmail.com>
2023-07-07 12:06:51 +0000

    update team

--- src/screens/team/Team.tsx
+++ src/screens/team/Team.tsx
@@ -2,11 +2,11 @@ import { ScreenContentLayout } from '@layouts/ScreenContentLayout';
 import { useRoute } from '@react-navigation/native';
 import { VueEnsemble } from './components/VueEnsemble';
 import { Matchs } from './components/Matchs';
-import {Effectif} from './components/Effectif'
+import { Effectif } from './components/Effectif';
 import { Statistiques } from './components/Statistiques';
 import {
   Text,
-  View,
+  View, 
   Image,
   ScrollView,
   TouchableOpacity,
@@ -38,68 +38,70 @@ export default function Team() {
   const [selectedOnglet, setSelectedOnglet] = useState<any>(onglet[0]);
   console.log(selectedOnglet);
 
-    return (
-      <View className="flex-1">
-        <Header
-          title={team_name}
-          showProfile={false}
-          showBackIcon
-        />
+  return (
+    <View className="flex-1">
+      <Header
+        title={team_name}
+        showProfile={false}
+        showBackIcon
+      />
 
-        <View className="">
-          <ImageBackground
-            className="h-36 w-full object-cover object-center opacity-30"
-            source={{
-              uri: team_flag,
-            }}
-          />
-          <View className="absolute items-center left-40 top-11 flex flex-col justify-center bg-orange-200 p-2 text-white">
-            <Text className="text-white">{team_name}</Text>
-            <Text className="uppercase text-white">
-              Groupe {team_group.label}
-            </Text>
-          </View>
-        </View>
-        <View className="mt-3 w-full ">
-          <ScrollView
-            className=""
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={{
-              columnGap: 8,
-            }}
-          >
-            {onglet.map((ong: any) => (
-              <TouchableOpacity
-                onPress={() => setSelectedOnglet(ong)}
-                className={`ml-1 rounded-full border border-primary px-3 py-[6px] ${
-                  ong.name == selectedOnglet.name
-                    ? 'bg-primary'
-                    : 'bg-transparent'
-                }`}
-              >
-                <Text
-                  className={`font-medium ${
-                    ong.name === selectedOnglet.name
-                      ? 'text-white'
-                      : 'text-primary'
-                  }`}
-                >
-                  {ong.name}
-                </Text>
-              </TouchableOpacity>
-            ))}
-          </ScrollView>
-        </View>
-        <View>
-          {
-            selectedOnglet.name === "Vue d'ensemble" ?<VueEnsemble name="Vue d'ensemble" /> :
-            selectedOnglet.name === "Matchs" ? <Matchs name="Matchs"/>:
-            selectedOnglet.name === "Effectif" ? <Effectif name="Effectifs"/> :<Statistiques name='PropStatistique'/>
-          }
+      <View className="">
+        <ImageBackground
+          className="h-36 w-full object-cover object-center opacity-30"
+          source={{
+            uri: team_flag,
+          }}
+        />
+        <View className="absolute left-40 top-11 flex flex-col items-center justify-center bg-orange-200 p-2 text-white">
+          <Text className="text-white">{team_name}</Text>
+          <Text className="uppercase text-white">
+            Groupe {team_group.label}
+          </Text>
         </View>
       </View>
-    );
-
- 
+      <View className="mt-3 w-full ">
+        <ScrollView
+          className=""
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            columnGap: 8,
+          }}
+        >
+          {onglet.map((ong: any) => (
+            <TouchableOpacity
+              onPress={() => setSelectedOnglet(ong)}
+              className={`ml-1 rounded-full border border-primary px-3 py-[6px] ${
+                ong.name == selectedOnglet.name
+                  ? 'bg-primary'
+                  : 'bg-transparent'
+              }`}
+            >
+              <Text
+                className={`font-medium ${
+                  ong.name === selectedOnglet.name
+                    ? 'text-white'
+                    : 'text-primary'
+                }`}
+              >
+                {ong.name}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </View>
+      <View>
+        {selectedOnglet.name === "Vue d'ensemble" ? (
+          <VueEnsemble name="Vue d'ensemble" />
+        ) : selectedOnglet.name === 'Matchs' ? (
+          <Matchs name="Matchs" />
+        ) : selectedOnglet.name === 'Effectif' ? (
+          <Effectif name="Effectifs" />
+        ) : (
+          <Statistiques name="PropStatistique" />
+        )}
+      </View>
+    </View>
+  );
 }
