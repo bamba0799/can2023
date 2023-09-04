56ce3b1be4064e69d774f2f705330b1aeddb3b87
David <david.sehi2016@gmail.com>
2023-09-04 19:52:49 +0000

    update

--- src/utils/match.ts
+++ src/utils/match.ts
@@ -1,38 +1,31 @@
-import { API_BASE_URL } from "@env";
-import { axiosInstance } from "@lib/axios";
+import { API_BASE_URL } from '@env';
+import { axiosInstance } from '@lib/axios';
 
+export const getMatchs = async () => {
+  const { data } = await axiosInstance.get(`${API_BASE_URL}/matchs`);
+  return data;
+};
 
-
-export const getMatchs = async() => {
-  const { data } = await axiosInstance.get(`${API_BASE_URL}/matchs`)
-  return data
-
-}
-
-export  async function fetchAllMatchs () {
-  let matchs
+export async function fetchAllMatchs() {
+  let matchs;
   try {
-   let matchsInitial = [{id:"0", label: "choissez un match"}]
-   let matchsApi = await getMatchs()
-   matchsApi = matchsApi.map((match: any) => {
+    let matchsInitial = [{ id: '0', label: 'choissez un match' }];
+    let matchsApi = await getMatchs();
+    matchsApi = matchsApi.map((match: any) => {
       return {
         id: match.id,
-        label: `${match.matchStats[0].team.name} - ${match.matchStats[1].team.name}`
-      }
-    })
-     matchs = matchsInitial.concat(matchsApi)
-    
-    
+        label: `${match.matchStats[0].team.name} - ${match.matchStats[1].team.name}`,
+      };
+    });
+    matchs = matchsInitial.concat(matchsApi);
   } catch (e: any) {
     console.log(e.message);
   }
-  return matchs
+  return matchs;
 }
 
-
-export const getDatePerMatchs = async(id:string) => {
-  const allMatchs =  await getMatchs()
-  const match = allMatchs.filter((match:any) => match.id === id )
-  return match
-
-}
+export const getDatePerMatchs = async (id: string) => {
+  const allMatchs = await getMatchs();
+  const match = allMatchs.filter((match: any) => match.id === id);
+  return match;
+};
