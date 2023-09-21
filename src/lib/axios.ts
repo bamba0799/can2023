da75b6155d2329182ce0ef47f7810a03effddb04
David Sehi <david.sehi2016@gmail.com>
2023-09-21 15:08:34 +0000

    add request interceptor

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,26 @@
 import axios from 'axios';
 import { API_BASE_URL } from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
 });
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    if (!config.headers['Authorization']) {
+      let isAuth = await AsyncStorage.getItem('token');
+      if (isAuth) {
+        const auth = JSON.parse(isAuth);
+        config.headers['Authorization'] = `Bearer ${auth.accessToken}`;
+      }
+    }
+    return config;
+  },
+  (e) => Promise.reject(e)
+);
+
+// TODO: add a response interceptor to automatically
+// refresh the access token when it expires
+
+export { axiosInstance };
